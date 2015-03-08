5937a07553b0c3dc4e94550ebe49c15becf7adc4
maht0rz <matej.sima@gmail.com>
2015-03-08 13:24:44 +0100

    Edit[Container]: Adding method documentation, alerting getByName to use getByRaw internally

--- src/GLDE/Container/Container.js
+++ src/GLDE/Container/Container.js
@@ -4,18 +4,39 @@ class Container {
         this.$content = content;
     }
 
+    /**
+     * Adds an item to container
+     * @param {mixed} item Item to be added to container
+     */
     add(item) {
         this.$content.push(item);
+        return this;
     }
 
+    /**
+     * Retrieves an item from container by index(-1)
+     * @param  {integer} index Position of item in container. WARNING: -1 compared to traditional array index.
+     * @return {mixed}   Item retrieved by index
+     */
     get(index){
         return this.$content[index-1];
     }
 
+    /**
+     * Removes an item from container by value
+     * @param  {mixed} item Item to be removed (by value) from container
+     * @return {[type]}      [description]
+     */
     remove(item) {
         this.$content.splice(this.$content.indexOf(item),1);
+        return this;
     }
 
+    /**
+     * Should search trough container and return items which fulfil requested rules.
+     * @param  {string} rules Conditions to be tested when matching container items.
+     * @return {Promise}
+     */
     $search(rules) {
         var self = this;
         return new Promise(function(resolve, reject){
@@ -31,10 +52,20 @@ class Container {
         })
     }
 
+    /**
+     * Returns container items (via promise) which match the required name
+     * @param  {string} name Name to be matched in items
+     * @return {array} Array of objects matching the rules
+     */
     getByName(name) {
-        return this.$search(`item.name == '${name}'`);
+        return this.getByRaw(`item.name == '${name}'`);
     }
 
+    /**
+     * Returns container items (via promise) which match the required rules
+     * @param  {string} query Rules to be matched within container items
+     * @return {Promise}
+     */
     getByRaw(query) {
         return this.$search(query);
     }
